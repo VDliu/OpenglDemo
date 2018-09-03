7be14f1697d7912e6f819c22020291605fd811f3
ge.liu <756384955@qq.com>
2018-09-03 21:17:34 +0800

    the 4 chapter ,add color and shader.

--- app/src/main/java/app/logo/com/opengldemo/AirHockeyRenderer.java
+++ app/src/main/java/app/logo/com/opengldemo/AirHockeyRenderer.java
@@ -3,11 +3,11 @@ package app.logo.com.opengldemo;
 import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
-import java.util.logging.SocketHandler;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -25,10 +25,14 @@ class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private static String TAG = "AirHockeyRenderer";
 
     //static section
-    private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int BYTES_PER_FLOAT = 4;
-    private static final String U_COLOR = "u_Color";
-    private int uColorLocation;
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int TOTAL_COMPONENT_COUNT = POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT;
+    private static final int STRIDE = TOTAL_COMPONENT_COUNT * BYTES_PER_FLOAT;
+
+    private static final String A_COLOR = "a_Color";
+    private int aColorLocation;
     private static final String A_POSITION = "a_Position";
     private int aPositionLocation;
 
@@ -39,26 +43,25 @@ class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private String fragmentShaderCode;
 
 
-
     private FloatBuffer vertexData = null;
     private float[] tableVerticesWithTriangles = {
-            // Triangle 1
-            -0.5f, -0.5f,
-            0.5f, 0.5f,
-            -0.5f, 0.5f,
-
-            //Trianle 2
-            -0.5f, -0.5f,
-            0.5f, -0.5f,
-            0.5f, 0.5f,
+            // define the desktop vertex：x y r g b
+            //前两个点为坐标，后面三个点代表顶点颜色
+            0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
 
             //Line 1
-            -0.5f, 0f,
-            0.5f, 0f,
+            -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
 
-            //Line 2
-            0f, -0.25f,
-            0f, 0.25f
+            //point 1
+            0.0f, -0.25f, 0.0f, 0.0f, 1.0f,
+            //point 2
+            0.0f, 0.25f, 1.0f, 0.0f, 0.0f
     };
 
     public AirHockeyRenderer(Context context) {
@@ -72,33 +75,38 @@ class AirHockeyRenderer implements GLSurfaceView.Renderer {
         vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT).
                 order(ByteOrder.nativeOrder()).asFloatBuffer();
         vertexData.put(tableVerticesWithTriangles);
-        vertexData.position(0);
 
         //获取shader glsl代码
-        vertexShaderCode = TextResourceReader.readTextFileFromResource(mContext,R.raw.simple_vertex_shader);
-        fragmentShaderCode = TextResourceReader.readTextFileFromResource(mContext,R.raw.simple_fragment_shader);
+        vertexShaderCode = TextResourceReader.readTextFileFromResource(mContext, R.raw.simple_vertex_shader);
+        fragmentShaderCode = TextResourceReader.readTextFileFromResource(mContext, R.raw.simple_fragment_shader);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        LogDebug.e(TAG, "onSurfaceCreated......");
         GLES20.glClearColor(0.0f, 0.0f, 0, 0);
+
         int vertexShader = ShaderHelper.complieVertexShader(vertexShaderCode);
         int framentShader = ShaderHelper.complieFragmentShader(fragmentShaderCode);
-        int programId = ShaderHelper.linkProgram(vertexShader,framentShader);
+
+        int programId = ShaderHelper.linkProgram(vertexShader, framentShader);
         //告诉Pengl 在绘制任何东西在屏幕上的时候要使用这里定义的程序
         ShaderHelper.validateProgram(programId);
         GLES20.glUseProgram(programId);
 
         //查找u_color 和a_Position位置
-        uColorLocation = GLES20.glGetUniformLocation(programId,U_COLOR);
-        aPositionLocation = GLES20.glGetAttribLocation(programId,A_POSITION);
+        aColorLocation = GLES20.glGetAttribLocation(programId, A_COLOR);
+        aPositionLocation = GLES20.glGetAttribLocation(programId, A_POSITION);
 
         //告诉OPengl去找到a_Position对应的数据
-
-        GLES20.glVertexAttribPointer(aPositionLocation,POSITION_COMPONENT_COUNT,GLES20.GL_FLOAT,
-                false,0,vertexData);
+        vertexData.position(0);
+        GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
+                false, STRIDE, vertexData);
         GLES20.glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        GLES20.glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT,
+                false, STRIDE, vertexData);
+        GLES20.glEnableVertexAttribArray(aColorLocation);
 
     }
 
@@ -110,27 +118,20 @@ class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        LogDebug.e(TAG, "onDrawFrame......");
         //清空屏幕，会使用glClearColor定义的颜色填充整个屏幕
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        //绘制顶点
 
         //初始一个白色桌面
-        GLES20.glUniform4f(uColorLocation,1.0f,1.0f,1.0f,1.0f);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES,0,6);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
 
         //绘制中心分割线
-        GLES20.glUniform4f(uColorLocation,1.0f,0f,0f,1.0f);
-        GLES20.glDrawArrays(GLES20.GL_LINES,6,2);
+        GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2);
 
         //绘制木槌代表的第一个点
-        GLES20.glUniform4f(uColorLocation,0.0f,0f,1.0f,1.0f);
-        GLES20.glDrawArrays(GLES20.GL_POINTS,8,1);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1);
 
         //绘制木槌代表的第二个点
-        GLES20.glUniform4f(uColorLocation,1.0f,0f,0.0f,1.0f);
-        GLES20.glDrawArrays(GLES20.GL_POINTS,9,1);
-
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1);
 
     }
 }
